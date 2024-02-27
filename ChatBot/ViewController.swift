faaa4b771e560ca00ffba88a81f953521b21fc0e
Gustavo <gustavo.gofran@gmail.com>
2024-02-27 17:36:09 -0300

    update

--- ChatBot/ViewController.swift
+++ ChatBot/ViewController.swift
@@ -8,10 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
-    
+
     // MARK: - Propiedades
-    
-    // Campo de texto para o user.
+
+    // Campo de texto para o usuário.
     private let field: UITextField = {
         let textField = UITextField()
         textField.placeholder = " Digite aqui..."
@@ -20,14 +20,14 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         textField.layer.cornerRadius = 20
         textField.clipsToBounds = true
         textField.returnKeyType = .done
-        
+
         let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.height))
         textField.leftView = paddingView
         textField.leftViewMode = .always
-        
+
         return textField
     }()
-    
+
     /// Exibição das mensagens.
     private let tableView: UITableView = {
         let tableView = UITableView()
@@ -35,54 +35,67 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         return tableView
     }()
-    
+
     /// Mensagens do chat.
-    private var messages: [String] = []
-    
+    private var messages: [(text: String, isUser: Bool)] = []
+
+    // Cor de fundo das respostas do bot.
+    private let botResponseBackgroundColor = UIColor.white //
+
     // MARK: - Métodos
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Adiciona a tabela e o campo de texto à hierarquia de visualização.
         view.addSubview(tableView)
         view.addSubview(field)
-        view.backgroundColor = .orange // estou mexendo aqui
-        tableView.backgroundColor = .orange // estou mexendo aqui
-        
+        view.backgroundColor = .orange
+        tableView.backgroundColor = .orange
+
         // Configuração dos delegates e datasources da tabela e do campo de texto.
         tableView.delegate = self
         tableView.dataSource = self
         field.delegate = self
-        
+
         // Configuração constraints.
         NSLayoutConstraint.activate([
             field.heightAnchor.constraint(equalToConstant: 50),
             field.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 20),
             field.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -20),
-            field.bottomAnchor.constraint(equalTo: view.keyboardLayoutGuide.topAnchor),
-            
+            field.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+
             tableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
             tableView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
             tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            tableView.bottomAnchor.constraint(equalTo: field.topAnchor)
+            tableView.bottomAnchor.constraint(equalTo: field.topAnchor, constant: -15)
         ])
     }
-    
+
     // MARK: - TableView
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return messages.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = messages[indexPath.row]
+
+        let message = messages[indexPath.row]
+        cell.textLabel?.text = message.text
+
+        if !message.isUser {
+            cell.backgroundColor = botResponseBackgroundColor
+        } else {
+            cell.backgroundColor = .white
+        }
+
         return cell
     }
-    
+
+
     // MARK: - TextField
-    
+
     // Mensagens para respostas
     let messageResponses: [String: String] = [
         "ola": "- Oi! Como posso ajudá-lo?",
@@ -90,30 +103,33 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         "tudo bem?": "- Estou bem, obrigado!",
         "gustavo francischini": "- Esse é o meu criador!",
         "qual seu nome?": "- Sou o ChatzinhoBot!"
-        
     ]
-    
+
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        if let text = textField.text, !text.isEmpty {
+        if let text = textField.text?.trimmingCharacters(in: .whitespacesAndNewlines), !text.isEmpty {
             // Pré-processamento da mensagem
             let processedText = text.preprocessarMensagem()
-            
+
             // Adiciona a mensagem do usuário ao array de mensagens.
-            messages.append(text)
-            
-            // Verifica se há uma resposta para a mensagem do user.
+            messages.append((text, true)) // Marca como mensagem do usuário
+
+            // Verifica se há uma resposta para a mensagem do usuário.
             if let response = messageResponses[processedText.lowercased()] {
-                messages.append(response)
+                messages.append((response, false)) // Marca como resposta do bot
             } else {
                 // Se não houve resposta..
-                messages.append("- Ainda não tenho resposta para isso =(")
+                messages.append(("- Ainda não tenho resposta para isso =(", false)) // Marca como resposta do bot
             }
-            
+
             // Atualiza a tabela e limpa o campo de texto.
             tableView.reloadData()
             textField.text = nil
+
+            // Rolando a tabela para a última mensagem.
+            let indexPath = IndexPath(row: messages.count - 1, section: 0)
+            tableView.scrollToRow(at: indexPath, at: .bottom, animated: true)
         }
-        
+
         return true
     }
 }
@@ -122,7 +138,7 @@ extension String {
     func removaAcentos() -> String {
         return folding(options: .diacriticInsensitive, locale: .current)
     }
-    
+
     func preprocessarMensagem() -> String {
         var mensagem = self.lowercased() // Converter para minúsculas
         mensagem = mensagem.removaAcentos() // Remover acentos
